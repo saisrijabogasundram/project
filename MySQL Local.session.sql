a91739414e4a7a5586f51dddaef8c4bf8fd6ffb9
saisrijabogasundram <saisrijabogasundram10@gmail.com>
2025-09-16 21:10:17 +0530

    Delete MySQL Local.session.sql

--- MySQL Local.session.sql
+++
@@ -1,6 +0,0 @@
-use LMS
-INSERT INTO courses(course_name, description, instructor_id, price)
-VALUES ('Python Basics', 'An introductory course to Python programming.', 3, 99.99);
-
-INSERT INTO courses(course_name, description, instructor_id, price)
-VALUES ('Data Science', 'An introductory course to Data Science.', 3, 149.99);
